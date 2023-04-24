1d4fdc69df2c9b5c6e265d1e3ef9b21295490c38
Cactus <ysjungman@gmail.com>
2023-04-24 12:33:32 +0900

    152_context_사용

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,10 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import HeaderCartButton from "./HeaderCartButton";
+import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
-const Header = props => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
@@ -12,7 +12,7 @@ const Header = props => {
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
-        <img src={mealsImage} alt="A table full of delicious food" />
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
